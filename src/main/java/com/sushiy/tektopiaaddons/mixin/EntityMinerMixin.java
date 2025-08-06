5c0a5abf1af811d1e9e6f974e877874610c027d6
XMxmx111 <740057300@qq.com>
2025-08-06 23:02:25 +0800

    Add stone tools

--- src/main/java/com/sushiy/tektopiaaddons/mixin/EntityMinerMixin.java
+++ src/main/java/com/sushiy/tektopiaaddons/mixin/EntityMinerMixin.java
@@ -3,6 +3,7 @@ package com.sushiy.tektopiaaddons.mixin;
 import com.sushiy.tektopiaaddons.TektopiaAddons;
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.datasync.DataParameter;
@@ -15,13 +16,17 @@ import net.tangotek.tektopia.ProfessionType;
 import net.tangotek.tektopia.entities.EntityFarmer;
 import net.tangotek.tektopia.entities.EntityMiner;
 import net.tangotek.tektopia.entities.EntityVillagerTek;
+import net.tangotek.tektopia.entities.crafting.Recipe;
 import net.tangotek.tektopia.storage.ItemDesire;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 import java.util.function.Predicate;
 
@@ -90,5 +95,42 @@ public abstract class EntityMinerMixin extends EntityVillagerTek {
     @Inject(method = "<clinit>", at = @At("TAIL"))
     private static void staticBlock(CallbackInfo ci) {
         MINE_STONE = EntityDataManager.createKey(EntityMiner.class, DataSerializers.BOOLEAN);
+    }
+
+    @Inject(method = "buildCraftSet", at = @At("RETURN"), cancellable = true, remap = false)
+    private static void buildCraftSetInject(CallbackInfoReturnable<List<Recipe>> cir) {
+        List<Recipe> recipes = new ArrayList();
+        List<ItemStack> ingredients = new ArrayList();
+        ingredients.add(new ItemStack(Item.getItemFromBlock(Blocks.COBBLESTONE), 3));
+        ingredients.add(new ItemStack(Item.getItemFromBlock(Blocks.LOG), 1, 99));
+        Recipe recipe = new Recipe(ProfessionType.MINER, "craft_stone_pickaxe", 2, new ItemStack(Items.STONE_PICKAXE, 1), ingredients, 1, 1, (v) -> v.getSkillLerp(ProfessionType.MINER, 9, 3), 1, (v) -> !hasBetterPick(v));
+        recipes.add(recipe);
+        recipes.addAll(cir.getReturnValue());
+        ingredients = new ArrayList();
+        ingredients.add(new ItemStack(Item.getItemFromBlock(Blocks.COBBLESTONE), 3));
+        ingredients.add(new ItemStack(Item.getItemFromBlock(Blocks.LOG), 1, 99));
+        recipe = new Recipe(ProfessionType.MINER, "craft_stone_axe", 2, new ItemStack(Items.STONE_AXE, 1), ingredients, 1, 1, (v) -> v.getSkillLerp(ProfessionType.MINER, 9, 3), 1, (v) -> hasBetterPick(v));
+        recipes.add(recipe);
+        ingredients = new ArrayList();
+        ingredients.add(new ItemStack(Item.getItemFromBlock(Blocks.COBBLESTONE), 2));
+        ingredients.add(new ItemStack(Item.getItemFromBlock(Blocks.LOG), 1, 99));
+        recipe = new Recipe(ProfessionType.MINER, "craft_stone_sword", 2, new ItemStack(Items.STONE_SWORD, 1), ingredients, 1, 1, (v) -> v.getSkillLerp(ProfessionType.MINER, 9, 3), 1, (v) -> hasBetterPick(v));
+        recipes.add(recipe);
+        ingredients = new ArrayList();
+        ingredients.add(new ItemStack(Item.getItemFromBlock(Blocks.COBBLESTONE), 2));
+        ingredients.add(new ItemStack(Item.getItemFromBlock(Blocks.LOG), 1, 99));
+        recipe = new Recipe(ProfessionType.MINER, "craft_stone_hoe", 2, new ItemStack(Items.STONE_HOE, 1), ingredients, 2, 3, (v) -> v.getSkillLerp(ProfessionType.MINER, 9, 3), 1, (v) -> hasBetterPick(v));
+        recipes.add(recipe);
+        cir.setReturnValue(recipes);
+    }
+
+    private static boolean hasBetterPick(EntityVillagerTek villager) {
+        List<ItemStack> weaponList = villager.getInventory().getItems(EntityMiner.getBestPick(villager), 1);
+        for (ItemStack stack : weaponList) {
+            if (stack.getItem() != Items.WOODEN_PICKAXE) {
+                return true;
+            }
+        }
+        return false;
     }
 }

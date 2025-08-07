b6e69211912921e450ac7262dc4831b6cce08671
XMxmx111 <740057300@qq.com>
2025-08-07 19:22:39 +0800

    Create StructureVillagePiecesMixin.java

---
+++ src/main/java/com/sushiy/tektopiaaddons/mixin/StructureVillagePiecesMixin.java
@@ -0,0 +1,24 @@
+package com.sushiy.tektopiaaddons.mixin;
+
+import net.minecraft.world.gen.structure.StructureVillagePieces;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
+import java.util.Random;
+
+@Mixin(value = StructureVillagePieces.class)
+public class StructureVillagePiecesMixin {
+
+    @Inject(method = "getStructureVillageWeightedPieceList", at = @At("RETURN"), cancellable = true)
+    private static void removeHouse1(Random random, int size, CallbackInfoReturnable<List<StructureVillagePieces.PieceWeight>> cir) {
+        List<StructureVillagePieces.PieceWeight> list = cir.getReturnValue();
+        list.removeIf(weight -> weight.villagePieceClass == StructureVillagePieces.House1.class);
+        list.removeIf(weight -> weight.villagePieceClass == StructureVillagePieces.House3.class);
+        list.removeIf(weight -> weight.villagePieceClass == StructureVillagePieces.House4Garden.class);
+        list.removeIf(weight -> weight.villagePieceClass == StructureVillagePieces.WoodHut.class);
+        cir.setReturnValue(list);
+    }
+}
